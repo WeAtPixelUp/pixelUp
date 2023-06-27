19c8391ad0a4244980c5d73448ad5d65c29cc1d9
Nishant <srnikhil3721@gmail.com>
2023-06-27 17:46:25 +0530

    layout code refactored

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
-"use client";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { useEffect } from "react";
 import AppHeader from "../layout/app-header";
 import AppFooter from "../layout/app-footer";
-import Head from "next/head";
+
+export const metadata = {
+  title: "Pixel Up",
+  description: "Service based web agency",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.min.js");
-  }, []);
-
   return (
     <html lang="en">
-      <Head>
-        <title>Pixel Up</title>
-      </Head>
       <body>
         <AppHeader />
         {children}
